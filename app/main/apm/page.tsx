e04bb960be55cdcc11137095ac913e639f74a5ad
ThisTimeNull <krsy0411@naver.com>
2025-11-07 17:37:43 +0900

    [refactor(layout)] RootLayout 수정

--- app/main/apm/page.tsx
+++ app/main/apm/page.tsx
@@ -1,12 +1,3 @@
-import { Header } from '@/components/common/app/Header';
-
 export default function APMPage() {
-  return (
-    <section>
-      <Header />
-      <section className="p-6">
-        <p className="text-zinc-600">여기에 APM 관련 UI를 구성하세요.</p>
-      </section>
-    </section>
-  );
+  return <p className="text-zinc-600">여기에 APM 관련 UI를 구성하세요.</p>;
 }
