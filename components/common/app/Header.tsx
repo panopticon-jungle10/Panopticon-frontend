a6a9fd1fe48c2f15613014fc3373718e4df3ab92
jen <jeon.jena.k@gmail.com>
2025-11-07 19:18:50 +0900

    [feat] : 경로 재수정

--- components/common/app/Header.tsx
+++ components/common/app/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   // APM 드롭다운 아이템
   const apmItems = [
     {
-      href: '/main/apm',
+      href: '/main/apm/services',
       label: 'Services',
       ariaLabel: 'Go to Services',
     },
@@ -34,7 +34,7 @@ export const Header = () => {
           <HeaderDropdown
             triggerIcon={<SiGoogleanalytics className="w-6 h-6 text-zinc-700" />}
             triggerLabel="APM menu"
-            triggerHref="/main/apm/services"
+            triggerHref="/main/apm"
             title="APM"
             items={apmItems}
           />
