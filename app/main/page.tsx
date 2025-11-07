0ab9f97f54978e834d4855a6ce69181885aaa84f
ThisTimeNull <90031820+krsy0411@users.noreply.github.com>
2025-11-07 15:18:31 +0900

    [feature(header)] 헤더 컴포넌트 구현 (#9)
    * [fix(mainLayout)] 스크린 사이즈 가로,세로 100% 유지하도록 수정
    
    * [chore(folder)] 폴더 구조 개선
    
    * [chore(logo)] 로고에 링크 추가
    
    * [feat(header)] 헤더 & 헤더-드롭다운 컴포넌트 구현
    
    * [feat(header)] 페이지에 헤더 적용
    
    * [chore(login)] 폴더명 변경
    
    * [refactor(page)] 각 섹션별 패딩 적용

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,8 +1,13 @@
+import { Header } from '@/components/common/app/Header';
+
 export default function mainHome() {
   return (
-    <section className="space-y-2">
-      <h1 className="text-2xl font-semibold">메인</h1>
-      <p className="text-zinc-600">본문</p>
-    </section>
+    <>
+      <Header />
+      <section className="p-6">
+        <h1 className="text-2xl font-semibold">메인</h1>
+        <p className="text-zinc-600">본문</p>
+      </section>
+    </>
   );
 }
