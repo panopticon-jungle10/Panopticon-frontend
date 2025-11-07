ac9582adfc5e5de0ca895d104bac2ab30a215aa7
ThisTimeNull <krsy0411@naver.com>
2025-11-07 14:29:12 +0900

    [feat(header)] 페이지에 헤더 적용

--- app/main/apm/page.tsx
+++ app/main/apm/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from '@/components/common/app/Header';
+
 export default function APMPage() {
   return (
     <section>
-      <h1 className="text-2xl font-semibold">APM</h1>
-      <p className="text-zinc-600 mt-1">여기에 APM 관련 UI를 구성하세요.</p>
+      <Header />
+      <p className="text-zinc-600">여기에 APM 관련 UI를 구성하세요.</p>
     </section>
   );
 }
