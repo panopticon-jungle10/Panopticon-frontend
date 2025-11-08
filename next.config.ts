898ae1d0f803079e8e4094d94292ebd6e0b2189b
jen <jeon.jena.k@gmail.com>
2025-11-09 00:24:38 +0900

    [refactor] 깃 충돌

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // 빌드 결과물을 정적 파일로 내보내기
+  output: 'standalone', // 빌드 결과물을 정적 파일로 내보내기
   trailingSlash: true,
   images: {
     unoptimized: true,
