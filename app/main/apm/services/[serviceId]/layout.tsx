939becc66613d0c9d0d059664dad1c197c7300ba
Jen-k <jeon.jena.k@gmail.com>
2025-11-08 15:19:38 +0900

    [Feat] : 사이드바 & 레이아웃 생성 (#21)
    * [feat] : UI 폴더구조
    
    * [feat] :
    
    1. 랜딩 페이지 추가
    2. 회원가입 & 사이드바 삭제
    
    * [feat] : 폴더 변경 dashboard -> main
    
    * [feat] : 로그인 화면 수정
    
    * [refactor] : function 이름 변경 (dashboard -> main)
    
    * [refactor] : component 폴더 세분화
    
    * [refactor] : 임시 로고 component로 빼기
    
    * [fix] : 깃허브, 구글 로고 깨짐 해결
    
    * [refactor] : agent 페이지 제거
    
    * [refactor] : 파일 이름 변경, props 수정
    
    * [feat] : APM 페이지 레이아웃 생성
    
    * [refactor] : 폴더 정리
    
    * [feat] : 아이콘 추가
    
    * [feat] : 라이브러리 & 폴더 변경
    
    * [refactor] : uninstall lucide-react
    
    * [feat] : 경로 변경 & 아이콘 변경
    
    * [feat] : 경로 재수정
    
    * [feat] : 아이콘 변경
    
    * [feat] : 사이드바 생성
    
    * [style] : 헤더와 메인 페이지 사이 여백 삭제
    
    * [docs] : 주석 추가(확인 필요 X)
    
    * [refactor] : 폴더 구조 수정
    
    * [refactor] : Input 정리
    
    * [refactor] : baseUrl 삭제
    
    * [refactor] : layout 여백 되돌리기
    
    * [refactor]  : 주석 제거

---
+++ app/main/apm/services/[serviceId]/layout.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '@/components/common/app/Sidebar';
+
+export default function ApmLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex">
+      {/* 좌측 사이드바 */}
+      <Sidebar />
+
+      {/* 본문 */}
+      <main className="flex-1 pl-64 p-6 bg-gray-50 min-h-screen">{children}</main>
+    </div>
+  );
+}
