19fcbf3b0e75f7900c3d978795502bbeaf2f3495
jen <jeon.jena.k@gmail.com>
2025-11-10 01:43:41 +0900

    [refactor] format

--- app/(authenticated)/main/apm/services/[serviceId]/layout.tsx
+++ app/(authenticated)/main/apm/services/[serviceId]/layout.tsx
@@ -5,17 +5,11 @@ export async function generateStaticParams() {
   return services.map((id) => ({ serviceId: id }));
 }
 
-export default function ApmLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ApmLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 pl-64 p-6 bg-gray-50 min-h-screen">
-        {children}
-      </main>
+      <main className="flex-1 pl-64 p-6 bg-gray-50 min-h-screen">{children}</main>
     </div>
   );
 }
