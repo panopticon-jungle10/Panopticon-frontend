0f51d2aaa4c36c13bb5125f2a1af525282c94e0e
ThisTimeNull <krsy0411@naver.com>
2025-11-11 15:01:48 +0900

    [fix(mocking)] 잘못 설정한 부분 수정

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // 프로덕션 빌드에서 MSW 관련 파일 제외
-  webpack: (config, { isServer, dev }) => {
-    // 프로덕션 빌드 시 MSW 관련 모듈 무시
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        // MSW를 빈 모듈로 대체 (프로덕션에서 번들 제외)
-        'msw/browser': false,
-        msw: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
