3016c5b3d3394de3958926b62190d7feb5274725
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-11-15 02:37:16 +0900

    [chore] : 필요없는 파일 정리

--- app/(authenticated)/setting/install/nodejs/page.tsx
+++
@@ -1,6 +0,0 @@
-import FrameworkInstallClient from '@/components/features/apps/install/FrameworkInstallClient';
-import { frameworksData } from '../frameworks';
-
-export default function NodejsInstallPage() {
-  return <FrameworkInstallClient framework={frameworksData.nodejs} />;
-}
