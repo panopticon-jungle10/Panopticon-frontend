563ddb84d8c8edc5dd7dfb7c5185ea1c4bb3c995
jen <jeon.jena.k@gmail.com>
2025-11-18 03:01:30 +0900

    format

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -19,11 +19,7 @@ export default function Logo({
     <div className={`flex items-center gap-2 ${className}`}>
       <Link href="/" className="flex items-center gap-2">
         <div className={`${iconSize} flex items-center justify-center`}>
-          <img
-            src="/logo/Logo.png"
-            alt="Panopticon"
-            className="w-full h-full object-contain"
-          />
+          <img src="/logo/Logo.png" alt="Panopticon" className="w-full h-full object-contain" />
         </div>
         {showText && <span className={`${textSize} font-semibold`}>Panopticon</span>}
       </Link>
